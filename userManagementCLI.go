93c5ca48e981b1dfe5cf028932e48c2368bdf828
pyxudev <xsq51py@gmail.com>
2025-09-20 15:14:05 +0800

    Changed go script name

--- userManagementCLI.go
+++ userManagementCLI.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -17,12 +19,13 @@ type User struct {
 }
 
 func main() {
+	// Load .env File
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	// 環境変数から読み込む
+	// Reade Environment Variables
 	user := os.Getenv("DB_USER")
 	pass := os.Getenv("DB_PASS")
 	host := os.Getenv("DB_HOST")
@@ -37,14 +40,36 @@ func main() {
 		panic("failed to connect database")
 	}
 
-	// テーブルのマイグレーション
+	// Table Migration
 	if err := db.AutoMigrate(&User{}); err != nil {
 		panic("failed to migrate: " + err.Error())
 	}
 
-	// GORMを使ったデータベース操作をここに記述
-	newUser := User{Name: "new_name", Email: "User@gmail.com"}
-	result := db.Create(&newUser) // レコードの挿入
+	newUser := []User{}
+
+	// --- CLI Enter Start ---
+	reader := bufio.NewReader(os.Stdin)
+	for {
+
+		fmt.Print("Enter UserName: ")
+		userName, _ := reader.ReadString('\n')
+		userName = strings.TrimSpace(userName)
+
+		if userName == "q" {
+			fmt.Println("User input ended.")
+			break
+		}
+
+		fmt.Print("Enter UserEmail: ")
+		email, _ := reader.ReadString('\n')
+		email = strings.TrimSpace(email)
+
+		newUser = append(newUser, User{Name: userName, Email: email})
+	}
+	// --- CLI Enter End ---
+
+	// CUDA Operations
+	result := db.Create(&newUser) // Insert Records
 
 	if result.Error != nil {
 		panic("Failed to insert database. Error Message: " + result.Error.Error())
@@ -53,12 +78,15 @@ func main() {
 		if err := db.First(&u).Error; err != nil {
 			panic(err)
 		}
-		fmt.Printf("First user: %+v\n", u)
 
-		var users []User
-		db.Find(&users)
-		fmt.Printf("All users: %+v\n", users)
+		// --- Activate if you need to see the first record ---
+		// fmt.Printf("First user: %+v\n", u)
 
-		fmt.Println("Done!")
+		// --- Activate if you need to see all records ---
+		// var users []User
+		// db.Find(&users)
+		// fmt.Printf("All users: %+v\n", users)
+
+		fmt.Println("User insert ended!")
 	}
 }
