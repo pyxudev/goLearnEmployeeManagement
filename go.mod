9f48698eefb9dcdaa27b98997c6790c066227db0
pyxudev <xsq51py@gmail.com>
2025-09-20 14:55:35 +0800

    add .gitginore and .env

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/text v0.29.0 // indirect
 	gorm.io/driver/mysql v1.6.0 // indirect
 	gorm.io/gorm v1.31.0 // indirect
--- test.go
+++ test.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID    uint
+	ID    uint `gorm:"primaryKey"`
 	Name  string
 	Email string
 }
 
 func main() {
-	dsn := "username:pass@tcp(127.0.0.1:3306)/study?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// 環境変数から読み込む
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, pass, host, port, name)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
@@ -22,19 +38,26 @@ func main() {
 	}
 
 	// テーブルのマイグレーション
-	db.AutoMigrate(&User{})
+	if err := db.AutoMigrate(&User{}); err != nil {
+		panic("failed to migrate: " + err.Error())
+	}
 
 	// GORMを使ったデータベース操作をここに記述
-	user := User{Name: "new_name", Email: "abc@gmail.com"}
-	result := db.Create(&user)
-	if result != nil {
-		panic("failed to insert database")
+	newUser := User{Name: "new_name", Email: "User@gmail.com"}
+	result := db.Create(&newUser) // レコードの挿入
+
+	if result.Error != nil {
+		panic("Failed to insert database. Error Message: " + result.Error.Error())
 	} else {
-		var user User
-		db.First(&user)
-		db.Where("name = ?", "new_name").Find(&user)
+		var u User
+		if err := db.First(&u).Error; err != nil {
+			panic(err)
+		}
+		fmt.Printf("First user: %+v\n", u)
+
 		var users []User
 		db.Find(&users)
+		fmt.Printf("All users: %+v\n", users)
 
 		fmt.Println("Done!")
 	}
